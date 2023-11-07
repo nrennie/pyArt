4586fafd1b4000fcac6e8bbedbd528e0cab7c686
nrennie <nrennie35@gmail.com>
2023-11-07 19:25:53 +0000

    make code more efficient

--- Python/nexus.py
+++ Python/nexus.py
@@ -7,21 +7,24 @@ def nexus(n_x, max_y, size, linewidth, bg_col, col_palette, s):
   """Generates plot of lines and points."""
   # generate data
   random.seed(s)
-  plot_data=pd.DataFrame({'x': [], 'y': [], 'grp': []})
+  # start and end points
+  random.seed(s)
+  n_y_start=random.choices(range(round(max_y/2)), k=n_x)
+  n_y_end=random.choices(range(round(max_y/2)+1, max_y + 1), k=n_x)
+  # get x and y co-ordinates in dataframe
+  x_list=[]
+  y_list=[]
   for i in range(n_x):
-      n_y_start=random.sample(range(round(max_y/2)), k=1)
-      n_y_end=random.sample(range(round(max_y/2)+1, max_y + 1), k=1)
-      plot_data_i=pd.DataFrame({'x': [i]*len(range(n_y_start[0], n_y_end[0])),
-      'y': range(n_y_start[0], n_y_end[0]),
-      'grp': [i]*len(range(n_y_start[0], n_y_end[0]))})
-      plot_data=pd.concat([plot_data, plot_data_i])
+    x_list.extend([i]*len(range(n_y_start[i], n_y_end[i])))
+    y_list.extend(range(n_y_start[i], n_y_end[i]))
+  plot_data = pd.DataFrame({'x': x_list, 'y': y_list})
   # choose colours
   cmap=mcolors.LinearSegmentedColormap.from_list('custom_cmap', col_palette, N=len(plot_data.index))
   plot_data['col']=[mcolors.to_hex(cmap(i)) for i in range(len(plot_data.index))]
   # plot data
   p = (pn.ggplot(plot_data) +
     pn.geom_line(
-      mapping=pn.aes(x="x", y="y", group="grp", colour="col"),
+      mapping=pn.aes(x="x", y="y", group="x", colour="col"),
       size=linewidth
     ) +
     pn.geom_point(
