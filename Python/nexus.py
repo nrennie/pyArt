722831499c6409e3e39f498a24b32d0e5efdea5a
nrennie <nrennie35@gmail.com>
2023-11-07 19:00:52 +0000

    fix max_y bug

--- Python/nexus.py
+++ Python/nexus.py
@@ -7,11 +7,10 @@ def nexus(n_x, max_y, size, linewidth, bg_col, col_palette, s):
   """Generates plot of lines and points."""
   # generate data
   random.seed(s)
-  max_y = max_y + 1
   plot_data=pd.DataFrame({'x': [], 'y': [], 'grp': []})
   for i in range(n_x):
       n_y_start=random.sample(range(round(max_y/2)), k=1)
-      n_y_end=random.sample(range(round(max_y/2)+1, max_y), k=1)
+      n_y_end=random.sample(range(round(max_y/2)+1, max_y + 1), k=1)
       plot_data_i=pd.DataFrame({'x': [i]*len(range(n_y_start[0], n_y_end[0])),
       'y': range(n_y_start[0], n_y_end[0]),
       'grp': [i]*len(range(n_y_start[0], n_y_end[0]))})
